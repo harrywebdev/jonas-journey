fb7097be3705b5a20951f1a1cc2da189a5f33598
Harry B <harry@gethornet.com>
2020-04-25 18:38:54 +0200

    Add styling and gitignore images

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,21 +13,45 @@
     <!-- Styles -->
     <style>
         html, body {
-            background-color: #fff;
+            background-color: #ecf0f1;
             color: #2c3e50;
             font-family: 'Source Serif Pro', serif;
-            font-weight: 200;
-            height: 100vh;
+            font-weight: 400;
             margin: 0;
+            font-size: 1.25em;
+            line-height: 1.75em;
+        }
+
+        h1 {
+            font-size: 2em;
+            line-height: 2em;
+            font-weight: 700;
+            margin-top: 0;
+            border-bottom: 1px solid #eee;
+        }
+
+        h2 {
+            font-size: 1.5em;
+            line-height: 1.5em;
+            font-weight: 600;
+            margin-top: 0;
         }
 
         a {
             color: #27ae60;
         }
 
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+        }
+
         .container {
-            max-width: 600px;
+            max-width: 900px;
             margin: 0 auto;
+            background: #fff;
+            padding: 1em;
         }
     </style>
 </head>
